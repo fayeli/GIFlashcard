fe5755bf338bf395b7993c73eea74fb4dc3292ad
Faye <lis@rose-hulman.edu>
2016-01-01 00:59:54 -0500

    Fix https issur

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 var app = angular.module('MainApp', ['ngMaterial'])
 			.run(function($log){
-        		$log.debug("MainApp running ");
+        		// $log.debug("MainApp running ");
             });
 
 app.controller('MainController', ['$scope', '$http', '$filter', 
 	function($scope, $http, $filter){
 		$scope.test = 'A fun & interactive language learning tool for creating flashcards with animated GIFs.';
-		$scope.gifurl = 'http://media1.giphy.com/media/3o8doVAxrMjXbIHaU0/200w.gif';
+		$scope.gifurl = '//media1.giphy.com/media/3o8doVAxrMjXbIHaU0/200w.gif';
 		$scope.translation = '狗';
 		$scope.vocab = 'dog';
 		$scope.querySearch = querySearch;
@@ -14,23 +14,23 @@ app.controller('MainController', ['$scope', '$http', '$filter',
 		$scope.searchTextChange = searchTextChange;
 
 		function querySearch(query){
-			return $http.get('http://api.pearson.com/v2/dictionaries/ldec/entries?headword=' + query)
+			return $http.get('//api.pearson.com/v2/dictionaries/ldec/entries?headword=' + query)
 					.then(function(response){
-      				console.log(response.data.results);
+      				// console.log(response.data.results);
       				return response.data.results;
     		});
 		};
 
 		function searchTextChange(text){
-			console.log('Text changed to ' + text);
+			// console.log('Text changed to ' + text);
 		};
 
 		function selectedItemChange(item){
-			console.log('Item selected is' + item.headword);
+			// console.log('Item selected is' + item.headword);
 			$scope.vocab = item.headword;
 			$scope.translation = item.senses[0].translation;
 
-			$http.get('http://api.giphy.com/v1/gifs/search?q=' + item.headword + '&limit=5&rating=g&api_key=dc6zaTOxFJmzC')
+			$http.get('//api.giphy.com/v1/gifs/search?q=' + item.headword + '&limit=5&rating=g&api_key=dc6zaTOxFJmzC')
     .success(function(data) {
     		var image = data.data[0].images.fixed_width;
     		if (image.height > 250){
